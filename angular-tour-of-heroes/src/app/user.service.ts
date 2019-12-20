d71e157a8d07b5a562312ee30899fba239a59dd3
Dante <dante.cornet@gmail.com>
2019-12-21 00:16:01 +0100

    dit zou moeten werken

--- angular-tour-of-heroes/src/app/user.service.ts
+++ angular-tour-of-heroes/src/app/user.service.ts
@@ -11,7 +11,7 @@ import { MessageService } from './message.service';
 @Injectable({ providedIn: 'root' })
 export class UserService {
 
-  private usersUrl = 'localhost:8080/Controller?action=';  // URL to web api
+  private usersUrl = 'http://localhost:8080/Controller?action=';  // URL to web api
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
